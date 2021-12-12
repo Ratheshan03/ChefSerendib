11c4a35757d5afa0aed07e8c45bd96102571bea9
Ratheshan03 <lionratheshan@gmail.com>
2021-12-12 06:49:13 +0530

    theme-added

--- packages/js/main.js
+++ packages/js/main.js
@@ -1,4 +1,4 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
@@ -19,7 +19,7 @@ if (navClose) {
   });
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
@@ -29,78 +29,159 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*=============== QUESTIONS ACCORDION ===============*/
-const accordionItems = document.querySelectorAll(".questions__item");
-
-accordionItems.forEach((item) => {
-  const accordionHeader = item.querySelector(".questions__header");
-
-  accordionHeader.addEventListener("click", () => {
-    const openItem = document.querySelector(".accordion-open");
-
-    toggleItem(item);
-
-    if (openItem && openItem !== item) {
-      toggleItem(openItem);
-    }
-  });
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover__container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 10,
+    slideShadows: true,
+  },
 });
 
-const toggleItem = (item) => {
-  const accordionContent = item.querySelector(".questions__content");
+/*==================== Countdown ====================*/
+const countdown = () => {
+  const countDate = new Date("Sep 25, 2021 00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
 
-  if (item.classList.contains("accordion-open")) {
-    accordionContent.removeAttribute("style");
-    item.classList.remove("accordion-open");
-  } else {
-    accordionContent.style.height = accordionContent.scrollHeight + "px";
-    item.classList.add("accordion-open");
-  }
+  // Time as normal units
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Converting our gap to normal units not ms
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  // Rendering countdown values into DOM
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".min").innerText = textMinute;
+  document.querySelector(".sec").innerText = textSecond;
 };
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
+// calling the function for every second (1000ms = 1s)
+setInterval(countdown, 1000);
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
+// Prize cards
+VanillaTilt.init(document.querySelectorAll(".image__overlay"), {
+  max: 12,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2,
+});
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id");
+/*==================== VIDEO ====================*/
+// const videoFile = document.getElementById("video-file"),
+//   videoButton = document.getElementById("video-button"),
+//   videoIcon = document.getElementById("video-icon");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
+// function playPause() {
+//   if (videoFile.paused) {
+//     // Play video
+//     videoFile.play();
+//     // We change the icon
+//     videoIcon.classList.add("ri-pause-line");
+//     videoIcon.classList.remove("ri-play-line");
+//   } else {
+//     // Pause video
+//     videoFile.pause();
+//     // We change the icon
+//     videoIcon.classList.remove("ri-pause-line");
+//     videoIcon.classList.add("ri-play-line");
+//   }
+// }
+// videoButton.addEventListener("click", playPause);
 
-/*=============== SHOW SCROLL UP ===============*/
+// function finalVideo() {
+//   // Video ends, icon change
+//   videoIcon.classList.remove("ri-pause-line");
+//   videoIcon.classList.add("ri-play-line");
+// }
+// // ended, when the video ends
+// videoFile.addEventListener("ended", finalVideo);
+
+/*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 400) scrollUp.classList.add("show-scroll");
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
 
-/*=============== DARK LIGHT THEME ===============*/
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+$("#menu").onePageNav({
+  currentClass: "active-link",
+  changeHash: false,
+  scrollSpeed: 750,
+  scrollThreshold: 0.5,
+  filter: "",
+  easing: "swing",
+});
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__social-link, .home__info,
+           .discover__container,
+           .experience__data, .image-container,
+           .place__card,
+           .experience__img-one,
+           .sponsor__content,
+           .footer__data, .footer__rights`,
+  {
+    origin: "top",
+    interval: 100,
+  }
+);
+
+sr.reveal(
+  `.about__data, 
+           .video__description,
+           .subscribe__description, 
+           .countdown__img-overlay,
+           .section-f`,
+  {
+    origin: "left",
+  }
+);
+
+sr.reveal(
+  `.about__img-overlay, 
+           .video__content,
+           .subscribe__form,
+           .countdown__data `,
+  {
+    origin: "right",
+    interval: 100,
+  }
+);
+
+/*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "ri-sun-line";
@@ -111,11 +192,11 @@ const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? "light" : "dark";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
-// We validate if the user previously chose a topic
+// We validate if the user previously choose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
@@ -136,20 +217,234 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
-  duration: 2500,
-  delay: 400,
-  // reset: true
+const University = document.querySelector("#university-input");
+const Name1 = document.querySelector("#name1-input");
+const Name2 = document.querySelector("#name2-input");
+const Name3 = document.querySelector("#name3-input");
+const Email1 = document.querySelector("#email1-input");
+const Email2 = document.querySelector("#email2-input");
+const Email3 = document.querySelector("#email3-input");
+const TeamLeader = document.querySelector("#leader-input");
+const TeamName = document.querySelector("#team-input");
+const TeamLeaderEmail = document.querySelector("#leaderEmail-input");
+const TeamContact = document.querySelector("#teamContact-input");
+const RotaractClub = document.querySelector("#Club-input");
+const Queries = document.querySelector("#query-input");
+const Merch = document.querySelector("#merchandise-input");
+
+// |------------------------------ Sliding Form (Slider) -------------------------------|
+
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+// ! First Next Button
+nextBtnFirst.addEventListener("click", function (event) {
+  if (
+    Name1.value == "" ||
+    Name1.value == null ||
+    Name2.value == "" ||
+    Name2.value == null
+  ) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Please check the missing name fields!",
+      footer: "<p>Participants name fields are compulsory.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else if (
+    Email1.value == "" ||
+    Email1.value == null ||
+    Email2.value == "" ||
+    Email2.value == null
+  ) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Email fields cannot be empty!",
+      footer: "<p>Please enter the correct email address.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else if (
+    !Email1.value.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) ||
+    !Email2.value.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    event.preventDefault();
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "You have entered the email field incorrectly",
+      footer:
+        "<p>Please check the fields and enter the valid email address!</p>",
+      background: "#edfffc",
+      showCancelButton: true,
+      showConfirmButton: false,
+      color: "fff",
+      timer: 5000,
+    });
+  } else {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-26%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+  }
 });
 
-sr.reveal(`.home__data`);
-sr.reveal(`.home__img`, { delay: 500 });
-sr.reveal(`.home__social`, { delay: 600 });
-sr.reveal(`.about__img, .contact__box`, { origin: "left" });
-sr.reveal(`.about__data, .contact__form`, { origin: "right" });
-sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
-  interval: 100,
+// ! Second Next button
+nextBtnSec.addEventListener("click", function (event) {
+  if (TeamLeader.value == "" || TeamLeader.value == null) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Team Leader name is missing!",
+      footer: "<p>Team Leader name field is compulsory.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else if (TeamName.value == "" || TeamName.value == null) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Team name cannot be empty!",
+      footer: "<p>Please enter your Team name.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else if (TeamLeaderEmail.value == "" || TeamLeaderEmail.value == null) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Team leader email cannot be empty!",
+      footer: "<p>Please enter the correct email address.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else if (
+    !TeamLeaderEmail.value.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    event.preventDefault();
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "You have entered the email field incorrectly!",
+      footer: "<p>Please check the field and enter a valid email address.</p>",
+      background: "#edfffc",
+      showCancelButton: true,
+      showConfirmButton: false,
+      color: "fff",
+      timer: 5000,
+    });
+  } else if (TeamContact.value == "" || TeamContact.value == null) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Team contact number field is empty!",
+      footer: "<p>Please enter a valid contact number.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  } else {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-52%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+  }
+});
+
+// ! Third Next Button
+nextBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-78%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+
+// ! Submit Button
+submitBtn.addEventListener("click", function (event) {
+  if (Merch.value == "" || Merch.value == null) {
+    event.preventDefault();
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Merchandise field is empty!",
+      footer: "<p>Please enter Yes or No on your preference.</p>",
+      showConfirmButton: false,
+      showCancelButton: true,
+      background: "#edfffc",
+      timer: 5000,
+    });
+  }
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+
+prevBtnSec.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+prevBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-26%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+prevBtnFourth.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-52%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
 });
